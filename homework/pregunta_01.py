5afebdee942476f2e4fca5cbf446900a1097c615
SaraBalbin <51859641+SaraBalbin@users.noreply.github.com>
2025-05-21 13:56:14 -0500

    Solucion laboratorio 4

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,7 +5,6 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
-
 def pregunta_01():
     """
     La información requerida para este laboratio esta almacenada en el
@@ -68,6 +67,46 @@ def pregunta_01():
     |  3 | Both operating profit and net sales for the three-month period increased , respectively from EUR16 .0 m and EUR139m , as compared to the corresponding quarter in 2006 | positive |
     |  4 | Tampere Science Parks is a Finnish company that owns , leases and builds office properties and it specialises in facilities for technology-oriented businesses         | neutral  |
     ```
-
-
     """
+    import pandas as pd
+    import zipfile
+    import os
+
+    def crear_dataframe(ruta_base):
+        datos = []
+        for carpeta_actual, subcarpetas, archivos in os.walk(ruta_base):
+            # print(f" Carpeta: {carpeta_actual}")
+            # print(f" Subcarpeta: {subcarpetas}")
+            # print(f" Archivo: {archivos}")
+            nombre_carp_actual = os.path.basename(carpeta_actual)
+            for archivo in archivos:
+                ruta_archivo = os.path.join(carpeta_actual, archivo)
+
+                with open(ruta_archivo, 'r', encoding='utf-8') as f:
+                        primera_linea = f.readline().strip()
+                        datos.append({
+                            'phrase': primera_linea,
+                            'target': nombre_carp_actual
+                        })
+        df = pd.DataFrame(datos)
+        return df
+    
+    def guardar_datos(output_ruta, df, nombre):
+        if not os.path.exists(output_ruta):
+            os.makedirs(output_ruta)
+        df.to_csv(f'{output_ruta}/{nombre}.csv', encoding='utf-8')
+         
+        
+    zip_ruta = 'files/input.zip'
+    input_ruta = 'files/input'
+    output_ruta = 'files/output'
+
+    if not os.path.exists(input_ruta):
+        with zipfile.ZipFile(zip_ruta, 'r') as zip_ref:
+            zip_ref.extractall('files')
+
+    df_test = crear_dataframe(f'{input_ruta}/test')
+    df_train = crear_dataframe(f'{input_ruta}/train')
+
+    guardar_datos(output_ruta, df_test, "test_dataset")
+    guardar_datos(output_ruta, df_train, "train_dataset")
